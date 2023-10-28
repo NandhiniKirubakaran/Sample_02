3c67cf01d9d4ba0bf89c669e8f6d803f57466ecf
Nandhini Kirubakaran <knaphasri@gmail.com>
2023-10-28 19:37:21 +0530

    add changes

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ window.onload = function() {
     // Display only the website name (domain)
     // document.getElementById('result').textContent = cleanedURL;
 
-fetch(`https://live2-3af8b-default-rtdb.asia-southeast1.firebasedatabase.app/websites/glittery-shortbread-77ca82netlifyapp.json`)
+fetch(`https://live2-3af8b-default-rtdb.asia-southeast1.firebasedatabase.app/websites/${cleanedURL}.json`)
 .then((value)=>value.json())
 .then((data)=>{
     abouttext.innerHTML=data.about_us;
